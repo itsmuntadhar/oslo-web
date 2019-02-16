aa8aa1cb685a602688c7af7c88e573c88518f071
Muntadhar Haydar <sparkist97@outlook.com>
2019-02-16 16:21:58 +0300

    fix db

--- oslo/urls.py
+++ oslo/urls.py
@@ -15,5 +15,5 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('rest-auth/', include('rest_auth.urls')),
     path('rest-auth/registration/', include('rest_auth.registration.urls')),
-    re_path(r'^(?:.*)/?$', include('reactapp.urls')),
+    re_path(r'^(?:.*)/?', include('reactapp.urls')),
 ]
